2902bc00cf59a8246a02583a606b7be98acdfe49
ShailendraSingh35461 <shailendrasingh35461@gmail.com>
2022-04-01 16:40:30 +0530

    aligned

--- yearlist.dart
+++ yearlist.dart
@@ -1,10 +1,9 @@
 import 'package:intl/intl.dart';
 void main() {
+List Year = [];
   final DateTime now = DateTime.now();
    final DateFormat formatter = DateFormat('yyyy');
   final int formatted = int.parse(formatter.format(now));
-  List Year = [];
-  //print(formatted);
   for(int i=2000;i<=formatted;i++){
     Year.add(i.toString());
   }
